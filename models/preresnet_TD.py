8a2837b844cdb8d875fef7920c33da40fa838b59
mengjian <jmeng15@asu.edu>
2020-02-29 03:43:22 -0700

    modified prereset model for target dropout

--- models/preresnet_TD.py
+++ models/preresnet_TD.py
@@ -16,17 +16,22 @@ def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=False)
 
+def conv3x3_td(in_planes, out_planes, stride=1, gamma=0.5, alpha=0.5, block_size=16):
+    return Conv2d_TD(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False, gamma=gamma, alpha=alpha, block_size=block_size)
 
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, gamma=0.5, alpha=0.5, block_size=16):
         super(BasicBlock, self).__init__()
         self.bn1 = nn.BatchNorm2d(inplanes)
         self.relu = nn.ReLU(inplace=True)
-        self.conv1 = conv3x3(inplanes, planes, stride)
+        # self.conv1 = conv3x3(inplanes, planes, stride)
+        self.conv1 = conv3x3_td(inplanes, planes, stride)
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv2 = conv3x3(planes, planes)
+        # self.conv2 = conv3x3(planes, planes)
+        self.conv2 = conv3x3_td(planes, planes)
         self.downsample = downsample
         self.stride = stride
 
@@ -55,13 +60,12 @@ class Bottleneck(nn.Module):
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
         self.bn1 = nn.BatchNorm2d(inplanes)
-        self.conv1 = Conv2d_TD(inplanes, planes, kernel_size=1, bias=False)
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv2 = Conv2d_TD(planes, planes, kernel_size=3, stride=stride,
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
                                padding=1, bias=False)
         self.bn3 = nn.BatchNorm2d(planes)
-        # self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-        self.conv3 = Conv2d_TD(planes, planes * 4, kernel_size=1, bias=False)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
@@ -91,7 +95,7 @@ class Bottleneck(nn.Module):
 
 class PreResNet(nn.Module):
 
-    def __init__(self, num_classes=10, depth=110):
+    def __init__(self, num_classes=10, depth=110, gamma=0.5, alpha=0.5, block_size=16):
         super(PreResNet, self).__init__()
         assert (depth - 2) % 6 == 0, 'depth should be 6n+2'
         n = (depth - 2) // 6
