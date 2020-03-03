3f941fbb5ec6103d9d63c5a9fafedc5ce5d3e55d
Jian Meng <jianmeng@jians-mbp.3500.dhcp.asu.edu>
2020-03-02 17:47:01 -0700

    Update: update the bash script, ready for evaluation.

---
+++ preresnet20_FP32_TD_eval.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+block_size=${1:-16}
+gamma=${2:-0.5}
+alpha=${3:-1.0}
+
+log_name="./logs/preresnet20_FP32_TD_${block_size}_${gamma}_${alpha}_eval.log" 
+save_file_name="preresnet20_FP32_TD_${block_size}_${gamma}_${alpha}_eval.pth" 
+
+python3 train.py --dataset CIFAR10 \
+                --data_path ./data \
+                --model PreResNet20_TD \
+                --log_file $log_name \
+                --save_file $save_file_name \
+                --block_size $block_size \
+                --TD_gamma $gamma \
+                --TD_alpha $alpha \
+                --epochs=200 \
+                --lr_init=0.1 \
+                --wd=5e-4 \
+                --weight-man 2 \
+                --evaluate \
+                --grad-man 2 \
+                --momentum-man 9 \
+                --activate-man 2 \
+                --error-man 2 \
+                --acc-man 9 \
+                --weight-rounding nearest \
+                --grad-rounding nearest \
+                --momentum-rounding stochastic \
+                --activate-rounding nearest \
+                --error-rounding nearest \
+                --acc-rounding stochastic \
+                --weight-exp 5 \
+                --grad-exp 5 \
+                --momentum-exp 6 \
+                --activate-exp 5 \
+                --error-exp 5 \
+                --acc-exp 6 \
+                --batch_size 128;
